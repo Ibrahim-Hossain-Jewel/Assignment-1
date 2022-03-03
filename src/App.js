a13c7a2e530b11909879e96d374a611cbd71b7cb
Ibrahim Hossain <ibrahimhossaincse@gmail.com>
2022-03-03 12:17:59 +0600

    split up the ExpenseItem component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import ExpenseItem from './components/ExpenseItem';
+import './components/expenseItem.css'
 
 function App() {
   const expenses = [
@@ -25,11 +26,13 @@ function App() {
   return (
     <div className="App">
       <h1>Let's get started the basic of React</h1>
-      <ExpenseItem title = {expenses[0].title} amount = {expenses[0].amount} date = {expenses[0].date }></ExpenseItem>
+      <div className='expense-item'>
+        <ExpenseItem title = {expenses[0].title} amount = {expenses[0].amount} date = {expenses[0].date }></ExpenseItem>
 
-      <ExpenseItem title = {expenses[1].title} amount = {expenses[1].amount} date = {expenses[1].date}></ExpenseItem>
+        <ExpenseItem title = {expenses[1].title} amount = {expenses[1].amount} date = {expenses[1].date}></ExpenseItem>
 
-      <ExpenseItem title = {expenses[2].title} amount = {expenses[2].amount} date = {expenses[2].date}></ExpenseItem>
+        <ExpenseItem title = {expenses[2].title} amount = {expenses[2].amount} date = {expenses[2].date}></ExpenseItem>
+      </div>
 
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,13 @@
 import React from "react";
+import ExpenseDate from "./ExpenseDate";
 import './expenseItem.css';
 
 function ExpenseItem({title,amount,date}){
-    const month = date.toLocaleString("en-US",{ month : 'long'});
-    const day = date.toLocaleString('en-us', { day : '2-digit'});
-    const year = date.getFullYear();
- 
+    
     return (
         //helper variable must be declare outside the function
         <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div>
+            <ExpenseDate date = {date} />
             <div className="expense-item__description">
                 <h1>{title}</h1>
                 <div className="expense-item__price">${amount}</div>

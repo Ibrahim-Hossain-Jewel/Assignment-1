8e507cdcd9a39ea2cd17ce6def7ba15215b9fe61
Ibrahim Hossain <ibrahimhossaincse@gmail.com>
2022-03-07 00:44:28 +0600

    passing data child to parent through function and props as well as function object container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import './App.css';
-import Expense from './components/expenses/Expenses'
+import Expense from './components/expenses/Expenses';
+import NewExpense from './components/expenses/NewExpense';
 const App = ()=>{
- 
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet paper',
+      amount: 94.12,
+      date: new Date(3,3,2022)
+    },
+    {
+      id: 'e2',
+      title: 'New TV',
+      amount: 777.12,
+      date: new Date(4,3,2022)
+    },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.12,
+      date: new Date(3,3,2022)
+    }
+  ];
+  const addExpenseHandler = (expense)=>{
+    console.log('these data are from app.js');
+      console.log(expense);
+  }
   return (
     <div className="App">
+          <NewExpense onExpenseHandler = {addExpenseHandler} />
           <Expense />
     </div>
   );
--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,7 +1,6 @@
 
 import ExpenseItem from './ExpenseItem'
 import './Expense.css';
-import NewExpense from './NewExpense';
 import Card from '../UI/Card';
 
 const Expense = ()=>{
@@ -25,10 +24,8 @@ const Expense = ()=>{
       date: new Date(3,3,2022)
     }
   ];
-
   return (
     <div>
-        <NewExpense />
         <Card className = "jewel">
           <ExpenseItem title = {expenses[0].title} amount = {expenses[0].amount} date = {expenses[0].date} />
           <ExpenseItem title = {expenses[1].title} amount = {expenses[1].amount} date = {expenses[1].date} />
